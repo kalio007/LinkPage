b801d2a48df350b896b37aa060a85bb2673200b7
Kalio007 <Kalioprince1@gmail.com>
2023-04-05 23:57:49 +0100

    height adjustments

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -4,7 +4,7 @@ import { RiTrademarkLine } from 'react-icons/ri'
 
 function MainPage() {
   return (
-    <div className="h-max w-full p-6 flex flex-col items-center mx-auto space-y-12 mt-15 max-w-md md:max-w-5xl md:space-y-2 md:p-10">
+    <div className="h-full w-full p-6 flex flex-col items-center mx-auto space-y-12 mt-15 max-w-md md:max-w-5xl md:space-y-2 md:p-10">
       
       <div className='bg-[#110CC1] shadow-3xl flex mt-20 md:mb-6 px-8 py-3'>
        <p className='text-[#FFFFFF] text-2xl  font-prime m-0 font-bold md:text-4xl '>futuredays </p>
@@ -14,7 +14,7 @@ function MainPage() {
         <div className='font-prime text-sm md:text-xl text-center'>
           <p clasName='text-[#000] text-sm text-center p-6 '>A loose crypto collective working to accelerate progress towards the future.</p>
         </div>
-        <div className='border-dotted border-[1px] border-black w-full p-6 h-96'>
+        <div className='border-dotted border-[1px] border-black w-full p-6 h-80'>
           <h2 className='text-lg text-[#110CC1] font-black my-3 font-prime md:text-xl' >PROJECTS</h2>
           <div className='font-prime  flex items-center'>
           <BiPlay color='#110CC1 ' size={15} />
